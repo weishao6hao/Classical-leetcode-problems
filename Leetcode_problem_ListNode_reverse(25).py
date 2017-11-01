ef36d10e464a68ca8b3eaae76469290fc58ceb3a
weishao6hao <798364373@qq.com>
2017-11-01 16:35:38 +0800

    Leetcode(25)
    Reverse Nodes in k-Group

---
+++ Leetcode_problem_ListNode_reverse(25).py
@@ -0,0 +1,22 @@
+#Leetcode problem(25): Reverse Nodes in k-Group
+class Solution:
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        dummy=jump=ListNode(0)
+        
+            count=0
+            while r and count<k:
+                count=count+1
+                r=r.next
+            if count==k:
+                cur=l
+                pre=r
+                for _ in range(k):
+                    cur.next,cur,pre=pre,cur.next,cur
+                jump.next,jump,l=pre,l,r
+            else:
+                return dummy.next
